0682178a9f7093d858cee9d2425029802774b655
Janne Vikstedt <vikstedt.janne@gmail.com>
2018-07-05 16:00:43 +0300

    If empty path is provided to json performer, just return everything as interface{}

--- internal/plugin/builtin/json.go
+++ internal/plugin/builtin/json.go
@@ -1,6 +1,8 @@
 package builtin
 
 import (
+	"encoding/json"
+
 	"github.com/jvikstedt/awake"
 	"github.com/tidwall/gjson"
 )
@@ -12,13 +14,22 @@ func (j JSON) Tag() string {
 }
 
 func (j JSON) Perform(scope awake.Scope) error {
-	json, _ := scope.ValueAsBytes("json")
+	data, _ := scope.ValueAsBytes("json")
 	path, _ := scope.ValueAsString("path")
 	returnName, _ := scope.ValueAsString("returnName")
 
-	result := gjson.GetBytes(json, path)
+	if path != "" {
+		result := gjson.GetBytes(data, path)
+		scope.SetReturnVariable(returnName, awake.MakeVariable(result.Value()))
+		return nil
+	}
 
-	scope.SetReturnVariable(returnName, awake.MakeVariable(result.Value()))
+	var result interface{}
+	if err := json.Unmarshal(data, &result); err != nil {
+		return err
+	}
+
+	scope.SetReturnVariable(returnName, awake.MakeVariable(result))
 
 	return nil
 }
