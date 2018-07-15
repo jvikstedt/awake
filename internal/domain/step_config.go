e7b79bc12fd07283b16a52344acf1a104af13666
Janne Vikstedt <vikstedt.janne@gmail.com>
2018-07-15 12:48:36 +0300

    ID and DisplayName for stepconfigs

--- internal/domain/step_config.go
+++ internal/domain/step_config.go
@@ -3,6 +3,8 @@ package domain
 import "github.com/jvikstedt/awake"
 
 type StepConfig struct {
+	ID              int    `json:"id"`
+	DisplayName     string `json:"displayName"`
 	Tag             `json:"tag"`
 	awake.Variables `json:"variables"`
 }
